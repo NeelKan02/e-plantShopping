c66364ff4b1e54304d15f9ac9681f7519bd1cc6b
Neelesh <neeleshkantiwal2002@gmail.com>
2025-01-06 11:53:28 -0500

    updated CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -26,7 +26,7 @@ export const CartSlice = createSlice({
         const { name, quantity} = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
-            itemToUpdate.quantity = quantity--;
+            itemToUpdate.quantity = quantity;
         }
     
     },
